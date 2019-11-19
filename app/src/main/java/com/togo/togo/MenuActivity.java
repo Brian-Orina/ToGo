58a9ed2fce6a2173f01afea83dbc7619e1a8e217
Brian-Orina <brianmogaka.bm@gmail.com>
2019-11-20 00:20:02 +0300

    Grid Layout Added On Menu Activity & Logout Is Successful on Menu Activity

--- app/src/main/java/com/togo/togo/MenuActivity.java
+++ app/src/main/java/com/togo/togo/MenuActivity.java
@@ -1,17 +1,59 @@
 package com.togo.togo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.GridView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MenuActivity extends AppCompatActivity {
+
+
+    GridLayout gridView;
+    CardView mProfile, mTrip, mHire, mHistory, mPasssword, mLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        gridView = (GridLayout)findViewById(R.id.gridView);
+
+        mProfile = (CardView) findViewById(R.id.edit_profile);
+        mTrip = (CardView) findViewById(R.id.trip_history);
+        mHire = (CardView) findViewById(R.id.hire_bike);
+        mHistory = (CardView) findViewById(R.id.hire_history);
+        mPasssword = (CardView) findViewById(R.id.change_password);
+        mLogout = (CardView) findViewById(R.id.logout);
+
+        mProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(MenuActivity.this, EditProfileActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(MenuActivity.this, SplashActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
+
+
 
     @Override
     public void onBackPressed() {
